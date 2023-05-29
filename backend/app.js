5b9e5dde804aefba91bb20fc1a48e61daad91f02
Rafael F Pessoa <112719981+RafaFonPessoa@users.noreply.github.com>
2023-05-29 15:08:56 -0300

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -23,70 +23,71 @@ app.use(express.urlencoded({ extended: true }));
 const User = require('./models/User');
 
 // Open Route - Public Route
-app.get('/', (req,res) => {
-    res.status(200).json({msg: 'Bem vindo a nossa API!'})
+app.get('/', (req, res) => {
+    res.status(200).json({ msg: 'Bem vindo a nossa API!' })
 })
 
 //Private Route
-app.get("user/:id", checkToken, async(req, res) => {
-    const id = req.params.id
+app.get("/user/:id", checkToken, async (req, res) => {
+    const id = req.params.id;
 
     // Check if user exists
-    const user = await User.findById(id, '-password')
+    const user = await User.findById(id, "-password");
 
-    if(!user) {
-        return req.status(404).json({msg:'Usuário não encontrado'})
+    if (!user) {
+        return res.status(404).json({ msg: "Usuário não encontrado" });
     }
 
-    req.status(200).json({user})
-}); 
+    res.status(200).json({ user });
+});
+
 
 function checkToken(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(" ")[1]
 
-    if(!token){
-        return res.status(401).json({msg:'Acesso negado!'})
+    if (!token) {
+        return res.status(401).json({ msg: 'Acesso negado!' })
     }
 
-    try{
+    try {
         const secret = process.env.SECRET
-        
+
         jwt.verify(token, secret)
-        
+
         next()
     }
-    catch(error){
-        res.status(400).json({msg:"Token Invalido!"})
+    catch (error) {
+        res.status(400).json({ msg: "Token Invalido!" })
     }
 }
 
 //Register User
-app.post('/auth/register', async(req,res) =>{
-    const{ name, email, password, confirmpassword } = req.body;
+app.post('/auth/register', async (req, res) => {
+    const { name, email, password, confirmpassword } = req.body;
 
     // Validations
-    if(!name) {
-        return res.status(422).json({msg:'O nome é obrigatório!'});
+    if (!name) {
+        return res.status(422).json({ msg: 'O nome é obrigatório!' });
     }
 
-    if(!email) {
-        return res.status(422).json({msg:'O email é obrigatório!'});
+    if (!email) {
+        return res.status(422).json({ msg: 'O email é obrigatório!' });
     }
 
-    if(!password) {
-        return res.status(422).json({msg:'A senha é obrigatório!'});
+    if (!password) {
+        return res.status(422).json({ msg: 'A senha é obrigatório!' });
     }
 
-    if(password !== confirmpassword) {
-        return res.status(422).json({msg: 'As senhas não conferem!'})
+    if (password !== confirmpassword) {
+        return res.status(422).json({ msg: 'As senhas não conferem!' })
     }
 
     // Check if user exists
-    const userExists = await User.findOne({email: email})
+    const userExists = await User.findOne({ email: email })
 
-    if(userExists) {
-        return res.status(422).json({msg: 'Email já cadastrado! por favor, utilize outro email!'})    
+    if (userExists) {
+        return res.status(422).json({ msg: 'Email já cadastrado! por favor, utilize outro email!' })
     }
 
     // Create password
@@ -100,13 +101,13 @@ app.post('/auth/register', async(req,res) =>{
         password: passwordHash,
     })
 
-    try{
+    try {
 
         await user.save()
 
-        res.status(201).json({msg: 'Usuário criado com sucesso!'})
+        res.status(201).json({ msg: 'Usuário criado com sucesso!' })
 
-    } catch(error) {
+    } catch (error) {
 
         console.log(error)
         res
@@ -119,48 +120,48 @@ app.post('/auth/register', async(req,res) =>{
 
 // Login User
 app.post("/auth/login", async (req, res) => {
-    const {email, password} = req.body
+    const { email, password } = req.body
 
     // Validations
 
-    if(!email) {
-        return res.status(422).json({msg:'O email é obrigatório!'});
+    if (!email) {
+        return res.status(422).json({ msg: 'O email é obrigatório!' });
     }
 
-    if(!password) {
-        return res.status(422).json({msg:'A senha é obrigatório!'});
+    if (!password) {
+        return res.status(422).json({ msg: 'A senha é obrigatório!' });
     }
 
     // Check if user exists
-    const user = await User.findOne({ email: email})
+    const user = await User.findOne({ email: email })
 
-    if(!user) {
-        return res.status(404).json({ msg: 'Usuário não encontardo!'})
+    if (!user) {
+        return res.status(404).json({ msg: 'Usuário não encontardo!' })
     }
 
     // Chack if password match
     const checkPassword = await bcrypt.compare(password, user.password)
 
-    if(!checkPassword) {
-        return res.status(404).json({ msg: 'Senha inválida!'})
+    if (!checkPassword) {
+        return res.status(404).json({ msg: 'Senha inválida!' })
     }
 
-    try{
+    try {
 
         const secret = process.env.SECRET;
 
         const token = jwt.sign(
             {
                 id: user._id,
-            }, 
+            },
             secret,
         )
 
-        res.status(200).json({msg: "Autenticação realizada com sucesso!", token})
+        res.status(200).json({ msg: "Autenticação realizada com sucesso!", token })
 
-    } catch(err) {
+    } catch (err) {
         console.log(error)
-        
+
         res
             .status(500)
             .json({
@@ -168,6 +169,28 @@ app.post("/auth/login", async (req, res) => {
             })
     }
 })
+
+// Delete User
+app.delete('/user/:id', checkToken, async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        // Verify if user exists
+        const user = await User.findById(id);
+
+        if (!user) {
+            return res.status(404).json({ msg: 'Usuário não encontrado' });
+        }
+
+        // Delete User
+        await user.remove();
+
+        return res.status(200).json({ msg: 'Usuário excluído com sucesso' });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ msg: 'Ocorreu um erro ao excluir o usuário' });
+    }
+});
 
 //Credenciais 
 const dbUser = process.env.DB_USER;
