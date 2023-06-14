141814d2729587293cf5596b19fbbadf494d2f06
Guthierre Teixeira <92392933+guthierremt@users.noreply.github.com>
2023-06-13 23:59:41 -0300

    Add files via upload

---
+++ apifilmes/controllers/pictureController.js
@@ -0,0 +1,52 @@
+const Picture = require("../models/Picture");
+
+const fs = require("fs");
+
+
+
+exports.create = async (req, res) => {
+    try {
+      const { name } = req.body;
+  
+      const file = req.file;
+      
+      const picture = new Picture({
+        name,
+        src: file.path,
+      });
+  
+      await picture.save();
+
+      res.json(picture);
+
+    } catch (err) {
+      res.status(500).json({ message: "Erro ao salvar a imagem." });
+    }
+  };
+
+  exports.findAll = async (req, res) => {
+    try {
+      const pictures = await Picture.find();
+      res.json(pictures);
+    } catch (err) {
+      res.status(500).json({ message: "Erro ao buscar as imagens." });
+    }
+  };
+
+  exports.remove = async (req, res) => {
+    try {
+      const picture = await Picture.findById(req.params.id);
+  
+      if (!picture) {
+        return res.status(404).json({ message: "Imagem não encontrada" });
+      }
+  
+      fs.unlinkSync(picture.src);
+  
+      await Picture.deleteOne({ _id: req.params.id });
+  
+      res.json({ message: "Imagem removida com sucesso" });
+    } catch (err) {
+      res.status(500).json({ message: "Erro ao remover a imagem" });
+    }
+  };
